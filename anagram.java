f362fbcc223d681a126fdf00b4eda08096b25df6
utkarshshrivastava2 <101251769+utkarshshrivastava2@users.noreply.github.com>
2022-03-15 00:08:30 +0530

    Update anagram.java

--- anagram.java
+++ anagram.java
@@ -3,35 +3,45 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class anagrams {
+public class Anagrams {
 
-	public static boolean isAnagram(String str1,String str2)
-	 {  
+	public  boolean isAnagram(String str1,String str2)
+	 {  // if length is not equal than they are not an anagram
 		boolean flag=false;
-		if(str1.length()!=str2.length()){
+		if(str1.length()!=str2.length())
 			return flag;
-			}
+			
 		else {
 			char[] Array1=str1.toCharArray();
 			char[] Array2=str2.toCharArray();
+		//sorting and then comparing every charcter of Array1 with Array2	
 			Arrays.sort(Array1);
 			Arrays.sort(Array2);
 			flag= Arrays.equals(Array1,Array2);
-			return flag;}
+			return flag;
+		}
 				
 
-  }
+  
+	}  
+	/* that two strings are anagram of each other or not
+     input-str1,str2
+     isAnagram will return a flag
+  
+  */
 
 		public static void  main(String[] args){
 			Scanner s= new Scanner(System.in);
 			String str1= s.nextLine();
 			String str2= s.nextLine();
-			if(isAnagram(str1,str2))
+			Anagrams a= new Anagrams();
+			if(a.isAnagram(str1,str2))
 			 System.out.println("yes it is an anagram");
 			else
 			System.out.println("no it is not an anagram");
 			
 		}
 
-	}
+	
+}
 	
